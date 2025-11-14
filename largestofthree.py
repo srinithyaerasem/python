c1823b0f1a74688048b0a3fd4934ec1d3475c114
srinithyaerasem <113082785+srinithyaerasem@users.noreply.github.com>
2025-11-14 22:45:54 +0530

    Create largestofthree.py

---
+++ largestofthree.py
@@ -0,0 +1,10 @@
+n1=(int(input("Enter a number1: ")))
+n2=(int(input("Enter a number2: ")))
+n3=(int(input("Enter a number3: ")))
+if(n1>n2 and n1>n3): 
+   print("Largest is ",n1)
+    
+elif(n2>n1 and n2>n3):
+    print("Largest is ",n2)
+else:
+    print("Largest is ",n3)
